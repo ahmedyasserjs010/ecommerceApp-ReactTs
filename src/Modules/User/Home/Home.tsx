840b6d87ddf6c4792ac5f4effecc4817087e975d
AhmedYasser_JS01 <ahmedyasser.js01@gmail.com>
2025-08-21 03:25:07 -0400

    test2

--- src/Modules/User/Home/Home.tsx
+++ src/Modules/User/Home/Home.tsx
@@ -3,11 +3,12 @@
 export default function Home() {
   return (
     <>
-        <div className="flex flex-col items-center justify-center h-screen dark:bg-gray-900 bg-white text-gray-800 dark:text-gray-200">
-            <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
-            <p className="text-lg text-center max-w-md">
-            This is the home page of our application. You can toggle dark mode using the button in the header.
-            </p>
+      <h1 className="text-4xl font-bold mb-4">Hi there!</h1>
+      <div className="flex flex-col items-center justify-center h-screen dark:bg-gray-900 bg-white text-gray-800 dark:text-gray-200">
+        <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
+        <p className="text-lg text-center max-w-md">
+          This is the home page of our application. You can toggle dark mode using the button in the header.
+        </p>
         </div>
     </>
   );
