2f97d8e543a17d5c06ba0594cc74febe039bc1ab
AhmedYasser_JS01 <ahmedyasser.js01@gmail.com>
2025-08-21 03:26:42 -0400

    Home Page

--- src/Modules/User/Home/Home.tsx
+++ src/Modules/User/Home/Home.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-4xl font-bold mb-4">Hi there!</h1>
+      <p className="text-lg mb-8">Welcome to the Home Page.</p>
       <div className="flex flex-col items-center justify-center h-screen dark:bg-gray-900 bg-white text-gray-800 dark:text-gray-200">
         <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
         <p className="text-lg text-center max-w-md">
